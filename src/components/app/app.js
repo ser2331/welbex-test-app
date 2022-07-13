9147c444865870df1f03b66adafc80422a658408
laptop <ser2331@gmail.com>
2022-07-14 02:35:53 +0300

    some settings

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -10,7 +10,7 @@ import './app.css';
 const { colsTypesMap, paramsTypesMap } = Types;
 
 const App = () => {
-    const baseApi = 'https://welbex-test-app.herokuapp.com/api';
+    const baseApi = 'https://welbex-test-app.herokuapp.com/api/list';
 
     const [data, setData] = useState([]);
 
